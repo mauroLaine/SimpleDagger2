a19fcac360ce04a795c8f074af06f5c13db01cf1
Mauricio Laine <laine.mau@gmail.com>
2019-09-08 15:51:53 -0700

    registered the application in Manifest, and inject SharedPreferences in MainActivity

--- app/src/main/java/com/laine/mauro/simpledagger2/MainActivity.java
+++ app/src/main/java/com/laine/mauro/simpledagger2/MainActivity.java
@@ -1,13 +1,29 @@
 package com.laine.mauro.simpledagger2;
 
+import android.content.SharedPreferences;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+
+import javax.inject.Inject;
 
 public class MainActivity extends AppCompatActivity {
+
+    private static final String LOGGER = "_MainActivity";
+
+    @Inject
+    SharedPreferences preferences;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        ((SimpleDagger2Application) getApplication()).getApplicationComponent().inject(this);
+
+        if (preferences != null) {
+            Log.d(LOGGER, "preferences is not null");
+        }
+
     }
 }
